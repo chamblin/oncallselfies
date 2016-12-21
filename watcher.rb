577542c71a0f83006501b60588e2059229334dd9
Cory Chamblin <c@pagerduty.com>
2016-12-21 09:57:47 -0600

    Ignore retweets

--- watcher.rb
+++ watcher.rb
@@ -5,7 +5,9 @@ store = SelfieStore.new(PStore.new("selfies.pstore"))
 
 with_streaming_client do |client|
   client.filter(:track => "#oncallselfie") do |tweet|
-    if tweet.media? and tweet.media.find{|m| m.is_a?(Twitter::Media::Photo)}
+    if tweet.media? &&
+       !(tweet.retweeted_status? || t.retweeted_tweet?) &&
+       tweet.media.find{|m| m.is_a?(Twitter::Media::Photo)}
       puts "#{Time.now} STORE: (tweet.id) @#{tweet.user.screen_name}: #{tweet.text}"
       store.add(tweet, true)
     else
